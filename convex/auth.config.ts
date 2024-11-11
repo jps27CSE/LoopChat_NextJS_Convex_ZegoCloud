b4f858daa1ee02e59f58b325e489db79455c9ed7
jps27CSE <jackpritomsoren@hotmail.com>
2024-11-11 15:26:52 +0600

    Clerk Auth Adding

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://just-egret-40.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
